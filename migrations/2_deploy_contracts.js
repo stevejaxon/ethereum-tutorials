68952d1c85481b8f553e8518cd52d59997bc5f41
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2017-04-06 11:28:09 +0100

    Adding an exploitable contract, a contract to perform the exploit and an initial tests to check that the stage is set correctly.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var Token = artifacts.require("./Token.sol");
+var HoneyPot = artifacts.require("./HoneyPot.sol");
+var HoneyBadger = artifacts.require("./HoneyBadger.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Token, 100, 'My Test Token', 0, '£');
+  deployer.deploy(HoneyPot, {value: web3.toWei(5, "Ether")});
 };
