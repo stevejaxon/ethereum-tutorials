179b7124c2443511a9f1b64db76f5a34d600a5aa
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-03-06 10:47:32 +0000

    Updating the code after lesson 5

--- contracts/cryptozombies/ZombieFeeding.sol
+++ contracts/cryptozombies/ZombieFeeding.sol
@@ -7,7 +7,7 @@ contract ZombieFeeding is ZombieFactory {
 
     KittyInterface kittyContract;
 
-    modifier ownerOf(uint _zombieId) {
+    modifier onlyOwnerOf(uint _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId]);
         _;
     }
@@ -24,7 +24,8 @@ contract ZombieFeeding is ZombieFactory {
         return (_zombie.readyTime <= now);
     }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) {
+    // 2. Change modifier name here as well
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModulus;
