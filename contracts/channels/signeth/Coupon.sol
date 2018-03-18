2b5b3badc8cc9a05e318faee717bf7052348327b
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-03-18 20:03:43 +0000

    Draft of a new approach

--- contracts/channels/signeth/Coupon.sol
+++ contracts/channels/signeth/Coupon.sol
@@ -14,10 +14,38 @@ contract Coupon is Ownable {
 
     using SafeMath for uint256;
 
-    mapping (address => uint) public balances;
-    mapping (address => mapping (address => uint256)) public allowed;
+    // TODO look at optimising size of data types later
+    struct PaymentChannel {
+        address owner;
+        uint value;             // Total value stored in the payment channel
+        uint stakeRequired;     // The amount of ETH required to be staked in order to attend
+        uint totalAttendees;
+        uint maxAttendees;
+        uint scheduledFor;    // When the event starts
+        uint settledBy;       // When the payment channel should be completely settled
+    }
 
-    event NewPriceSet(uint price);
+    mapping (address => uint) public balances;
+    mapping (address => PaymentChannel) public channels;
+
+    event RegistrationComplete(address indexed eventId, address attendee, uint attendeesListSize, uint stake);
+
+    function registerEvent(address _eventContract, uint _stakeRequired, uint _maxAttendees, uint _scheduledFor, uint _settledBy) external {
+        require(channels[_eventContract].owner == address(0));
+        require(_maxAttendees > 1);
+        require(_scheduledFor > now);
+        require(_settledBy > _scheduledFor);
+
+        channels[_eventContract] = PaymentChannel({
+            owner: msg.sender,
+            value: 0,
+            stakeRequired: _stakeRequired,
+            totalAttendees: 0,
+            maxAttendees: _maxAttendees,
+            scheduledFor: _scheduledFor,
+            settledBy: _settledBy
+            });
+    }
 
     // TODO digital signature verification
     function transferFrom(address _from, address _to, uint256 _value, address _approver, uint8 _v, bytes32 _r, bytes32 _s) external {
@@ -31,22 +59,33 @@ contract Coupon is Ownable {
         // Transfer(_from, _to, _value);
     }
 
-    // TODO think about timelocking staked value
-    // TODO think about how you ensure that only upto the current balance can be staked
-    function approve(address _eventContract, uint256 _value) external {
-        allowed[msg.sender][_eventContract] = _value;
-        // Approval(msg.sender, _spender, _value);
+    // TODO think about how to prevent an address registering more than once
+    function register(address _eventContract, uint256 _value) external payable {
+        require(channels[_eventContract].owner != address(0));
+        require(_value == msg.value);
+        PaymentChannel storage channel = channels[_eventContract];
+        require(channel.totalAttendees.add(1) <= channel.maxAttendees);
+        require(msg.value == channel.stakeRequired);
+        require(channel.value.add(msg.value) > channel.value);
+
+        channel.totalAttendees = channel.totalAttendees.add(1);
+        channel.value = channel.value.add(msg.value);
+
+        RegistrationComplete(_eventContract, msg.sender, channel.totalAttendees, msg.value);
     }
 
     function() public payable {
         balances[msg.sender] = balances[msg.sender].add(msg.value);
     }
 
-    // TODO how to prevent someone from pulling the plug to avoid losing their stake?
     function withdraw() external {
         uint amount = balances[msg.sender];
         balances[msg.sender] = 0;
-        delete allowed[msg.sender];
         msg.sender.transfer(amount);
+    }
+
+    function getChannelsDetails(address _eventContract) external view returns (address, uint, uint, uint, uint, uint, uint) {
+        PaymentChannel memory channel = channels[_eventContract];
+        return (channel.owner, channel.value, channel.stakeRequired, channel.totalAttendees, channel.maxAttendees, channel.scheduledFor, channel.settledBy);
     }
 }
