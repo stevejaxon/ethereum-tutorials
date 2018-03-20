921cec7409323b31d4e6294d0383cc0b30935a80
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-03-20 17:05:05 +0000

    Continued testing of the coupon contract

--- contracts/channels/signeth/Coupon.sol
+++ contracts/channels/signeth/Coupon.sol
@@ -7,7 +7,12 @@ pragma solidity ^0.4.0;
  * "Off-Chain Whitelist with On-Chain Verification for Ethereum Smart Contracts" article by Philippe Castonguay.
  */
 contract Coupon {
-    function purchase() public payable;
-    function withdraw(uint amount) public;
-    function transferFrom(address _from, address _to, uint256 _value, address _approver, uint8 _v, bytes32 _r, bytes32 _s) public;
+
+    event Deposited(address _account, uint _amount);
+    event Withdrawn(address _account, uint _amount);
+
+    function balanceOf(address _account) public returns (uint);
+    function deposit() public payable;
+    function withdraw(uint _amount) public;
+    // function transferFrom(address _from, address _to, uint256 _value, address _approver, uint8 _v, bytes32 _r, bytes32 _s) public;
 }
--- test/channels/signeth/testCoupon.js
+++ test/channels/signeth/testCoupon.js
@@ -17,22 +17,87 @@ contract('Coupon', async (accounts) => {
         });
     });
 
-    describe('Purchase tests', () => {
-        it('should be possible to purchase coupons in one single transfer', async () => {
+    describe('deposit tests', () => {
+        it('should be possible to deposit coupons in one single transfer', async () => {
+            // Setup the test
+            let depositor = accounts[1];
+            let amountToDeposit = web3.toWei(1, 'ether');
 
+            // test
+            try {
+                await couponInstance.deposit({from: depositor, value: amountToDeposit});
+            } catch(e) {
+                throw e;
+            }
+
+            // verify
+            let amountDeposited = await couponInstance.balanceOf.call(depositor);
+            assert.equal(amountDeposited, amountToDeposit)
         });
 
-        it('should be possible to incrementally purchase coupons in multiple transfers', async () => {
+        it('should be possible to incrementally deposit coupons in multiple transfers', async () => {
+            // Setup the test
+            let depositor = accounts[2];
+            let amountToDeposit1 = web3.toBigNumber(web3.toWei(1, 'kwei'));
+            let amountToDeposit2 = web3.toBigNumber(web3.toWei(27, 'gwei'));
+            let amountToDeposit3 = web3.toBigNumber(web3.toWei(716, 'gwei'));
+            let amountToDeposit4 = web3.toBigNumber(web3.toWei(82, 'finney'));
 
+            // test
+            try {
+                await couponInstance.deposit({from: depositor, value: amountToDeposit1});
+                await couponInstance.deposit({from: depositor, value: amountToDeposit2});
+                await couponInstance.deposit({from: depositor, value: amountToDeposit3});
+                await couponInstance.deposit({from: depositor, value: amountToDeposit4});
+            } catch(e) {
+                throw e;
+            }
+
+            // verify
+            let amountDeposited = await couponInstance.balanceOf.call(depositor);
+            let amountExpected = amountToDeposit1.add(amountToDeposit2).add(amountToDeposit3).add(amountToDeposit4);
+
+            assert.isTrue(amountDeposited.cmp(amountExpected) === 0);
         });
     });
 
     describe('Withdraw tests', () => {
         it('should be possible to withdraw all ether in on single transaction', async () => {
+            // Setup
+            let depositor = accounts[1];
+            let deposit = web3.toBigNumber(web3.toWei(13796, 'kwei'));
+            await couponInstance.deposit({from: depositor, value: deposit});
+            let totalDeposited = await couponInstance.balanceOf.call(depositor);
+
+            // Test
+            try {
+                await couponInstance.withdraw(totalDeposited, {from: depositor});
+            } catch(e) {
+                throw e;
+            }
+
+            // Verify
+            let remainingDeposited = await couponInstance.balanceOf.call(depositor);
+            assert.equal(remainingDeposited, 0, 'Expected that the user would have withdrawn all of their ether');
+        });
+
+        it('should be possible to withdraw part of the ether in on single transaction', async () => {
 
         });
 
         it('should be possible to withdraw all ether over the course of multiple transactions', async () => {
+
+        });
+
+        it('should be possible to withdraw part of the ether over the course of multiple transactions', async () => {
+
+        });
+
+        it('should not be possible to withdraw more ether than is available (not staked)', async () => {
+
+        });
+
+        it('should not be possible to withdraw more ether than is available (when some is staked)', async () => {
 
         });
     })
